d0b20beda83e91accc3ec8f782c878a94dec13c9
Alex w <weisea3@rpi.edu>
2016-10-09 15:24:49 -0400

    I swear if this doesn't work...

---
+++ Wish You Well main/Assets/Mole stuff/Detection.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Detection : MonoBehaviour {
+    GameObject mole;
+    public float offset;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Awake()
+    {
+         mole = this.transform.parent.gameObject;
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            
+            mole.transform.Translate(0f, offset, 0f);
+            
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if(coll.gameObject.tag == "Player")
+        {
+            mole.transform.Translate(0f, -offset, 0f);
+
+        }
+        
+    }
+}
